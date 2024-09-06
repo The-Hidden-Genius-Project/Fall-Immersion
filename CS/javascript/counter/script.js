ada80c0eb818e4c5338a8f3e15cf6b47e6f3c9ba
Abe <admin@Abraham-Keletas-Macbook-Pro.local>
2024-09-06 15:08:12 -0700

    finished counter app

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,4 +1,5 @@
 function counter() {
 // Write your code below. Hint: you can finish it in about 3-5 lines.
-
+    counter_number = document.getElementById('counter-number');
+    counter_number.innerHTML = parseInt(counter_number.innerHTML) + 1;
 }
