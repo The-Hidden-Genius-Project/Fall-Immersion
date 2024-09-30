2fb383e5efd65474127748df46803c2fc7de9a60
DeeBeeBoutaBag <159593266+DeeBeeBoutaBag@users.noreply.github.com>
2024-09-30 13:24:22 -0700

    Update script.js

--- CS/bootstrap/Sports-App/script.js
+++ CS/bootstrap/Sports-App/script.js
@@ -1,1 +1,10 @@
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 
+// Genius can add more JavaScript functionality here if needed
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Page Loaded Successfully.");
+});
