b71a5c6aee15d03006e8f2bc2ef28bdad670275f
Guy-365 <amarnie.edwards@hgs.hiddengeniusproject.org>
2024-10-29 11:07:52 -0400

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Get modal elements
+  
     const modal = document.getElementById("myModal");
     const openModalButton = document.getElementById("openModal");
     const closeModalButton = document.getElementById("closeModal");
 
-    // Event listener to open modal
     openModalButton.addEventListener("click", () => {
         modal.style.display = "block";
     });
 
-    // Event listener to close modal when clicking on the 'X' button
+   
     closeModalButton.addEventListener("click", () => {
         modal.style.display = "none";
     });
 
-    // Event listener to close modal when clicking outside the modal content
+    
     window.addEventListener("click", (event) => {
         if (event.target == modal) {
             modal.style.display = "none";
